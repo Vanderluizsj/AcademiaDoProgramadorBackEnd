7c01cf7ea1cf15d85f924f37f6490077480b3306
Vander <vanderluizsj@gmail.com>
2026-04-09 17:35:55 -0300

    V1 do jogo dos dados

---
+++ CorridaDeDados/CorridaDeDados.ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using CorridaDeDados.ConsoleApp;
+
+string jogador;
+Funcoes funcoes = new Funcoes();
+
+System.Console.Write("Digite seu nome para iniciar: ");
+//jogador = Console.ReadLine();
+
+System.Console.WriteLine();
+
+//System.Console.WriteLine($"Bem vindo {jogador}!");
+
+jogador = funcoes.startGame();
+
+
+System.Console.WriteLine($"\nO vencedor foi {jogador}");
